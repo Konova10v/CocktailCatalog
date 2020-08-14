877b0ff677377165373041575b7cd97a6aecaec3
Konova10v <43870971+Konova10v@users.noreply.github.com>
2020-08-14 10:26:02 +0500

    Add files via upload

---
+++ CocktailCatalog/CocktailCatalog/CocktailCatalog/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  CocktailCatalog
+//
+//  Created by Кирилл Коновалов on 14.08.2020.
+//  Copyright © 2020 Кирилл Коновалов. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
